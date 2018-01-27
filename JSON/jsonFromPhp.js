cea7e58a049dcd26c289bd6e9cd3f23fbcd1c70b
vincseize <vincseize@gmail.com>
2018-01-27 11:26:09 +0100

    Create jsonFromPhp.js

---
+++ JSON/jsonFromPhp.js
@@ -0,0 +1,46 @@
+/*
+The code below ...
+*/
+
+
+/* json.php *∕
+<?php
+$json = '[{"Language":"jQuery","ID":"1"},{"Language":"C#","ID":"2"},
+                           {"Language":"PHP","ID":"3"},{"Language":"Java","ID":"4"},
+                           {"Language":"Python","ID":"5"},{"Language":"Perl","ID":"6"},
+                           {"Language":"C++","ID":"7"},{"Language":"ASP","ID":"8"},
+                           {"Language":"Ruby","ID":"9"}]';
+echo json_encode($json);
+?>
+
+
+/* json.js *∕
+<script>
+var url = "json.php";
+$.ajax({
+        type: "GET", 
+        url: url, 
+        success: function(response)
+        {
+            /*response = '[{"Language":"jQuery","ID":"1"},{"Language":"C#","ID":"2"},
+                           {"Language":"PHP","ID":"3"},{"Language":"Java","ID":"4"},
+                           {"Language":"Python","ID":"5"},{"Language":"Perl","ID":"6"},
+                           {"Language":"C++","ID":"7"},{"Language":"ASP","ID":"8"},
+                           {"Language":"Ruby","ID":"9"}]'*/
+            // console.log(response);
+            
+	          var json_obj = $.parseJSON(response);//parse JSON
+            
+            var output="<ul>";
+            for (var i in json_obj) 
+            {
+                output+="<li>" + json_obj[i].Language + ",  " + json_obj[i].ID + "</li>";
+                console.log(json_obj[i].Language + ",  " + json_obj[i].ID);
+            }
+            output+="</ul>";
+            
+            $('span').html(output);
+        },
+        dataType: "json"//set to JSON    
+}) 
+</script>
