9a110fba1357505a193e8a48d66949a889602c38
vincseize <vincseize@gmail.com>
2018-02-13 00:21:07 +0100

    Add files via upload

--- LAYOUTS/logon_cookies.php
+++ LAYOUTS/logon_cookies.php
@@ -4,11 +4,15 @@
 </head>
 <body>
   <h2>User Login </h2>
+  toto / toto <br><br>
   <form name="login" method="post" action="login_cookies.php">
    Username: <input type="text" name="username"><br>
    Password: <input type="password" name="password"><br>
+   <br><br>
    Remember Me: <input type="checkbox" name="rememberme" value="1"><br>
+   <br><br>
    <input type="submit" name="submit" value="Login!">
+   
   </form>
 </body>
 </html>
