cffb94e8df86ee126ebe007b44fcb52f21e1158d
vincseize <vincseize@gmail.com>
2018-02-13 00:25:35 +0100

    Add files via upload

--- LAYOUTS/login_cookies.php
+++ LAYOUTS/login_cookies.php
@@ -1,48 +1,18 @@
-<?php
-
-$domain =  $_SERVER['SERVER_NAME'];
-// 'www.example.com'
-// echo $domain; 
-// '/account'
-$sub_domain =  '/LAYOUT';
-
-$path_login_ok = 'login_cookies_ok.php';
-
-/* These are our valid username and passwords */
-$user = 'toto';
-$pass = 'toto';
-
-
-
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    
-    if (($_POST['username'] == $user) && ($_POST['password'] == $pass)) {    
-        
-        if (isset($_POST['rememberme'])) {
-            /* Set cookie to last 1 year */
-            setcookie('username', $_POST['username'], time()+60*60*24*365, $sub_domain, $domain);
-            setcookie('password', md5($_POST['password']), time()+60*60*24*365, $sub_domain, $domain);
-        
-        } else {
-            /* Cookie expires when browser closes */
-            setcookie('username', $_POST['username'], false, $sub_domain, $domain);
-            setcookie('password', md5($_POST['password']), false, $sub_domain, $domain);
-        }
-        header('Location: '.$path_login_ok);
-        
-    } else {
-            echo 'Username/Password Invalid';
-            echo '<br><br>';
-            echo '<button onclick="goBack()">Go Back</button>';
-
-            echo '<script>';
-            echo 'function goBack() {';
-                echo 'window.history.back();';
-            echo '}';
-            echo '</script> ';
-    }
-    
-} else {
-    echo 'You must supply a username and password.';
-}
-?>
+<html>
+<head>
+<title>User Logon</title>
+</head>
+<body>
+  <h2>User Login </h2>
+  toto / toto <br><br>
+  <form name="login" method="post" action="login_cookies_check.php">
+   Username: <input type="text" name="username"><br>
+   Password: <input type="password" name="password"><br>
+   <br><br>
+   Remember Me: <input type="checkbox" name="rememberme" value="1"><br>
+   <br><br>
+   <input type="submit" name="submit" value="Login!">
+   
+  </form>
+</body>
+</html>
