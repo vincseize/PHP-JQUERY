70f3994d1f06a4a6e76edd27a46438f5c5c22232
vincseize <vincseize@gmail.com>
2018-03-13 20:51:10 +0100

    Create layout_header_footer_split4.php

---
+++ LAYOUTS/layout_header_footer_split4.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title> Flexbox 5 Panel Page Layout </title>
+  
+  
+  
+      <link rel="stylesheet" href="css/style_resize.css">
+
+  
+</head>
+
+<body>
+
+  <div class="flex-master">
+
+  <header class="page-header">
+    This is a header
+  </header>
+
+
+  <div class="page-content">
+    <nav class="sidebar-left" style="width:80%">
+      <h3>Sidebar</h3>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+        <li>Item 6</li>
+        <li>Item 7</li>
+        <li>Item 8</li>
+        <li>Item 9</li>
+        <li>Item 10</li>
+        <li>Item 11</li>
+        <li>Item 12</li>
+        <li>Item 13</li>
+        <li>Item 14</li>
+        <li>Item 15</li>
+        <li>Item 16</li>
+        <li>Item 17</li>
+        <li>Item 19</li>
+      </ul>
+    </nav>
+
+    <div class="splitter">
+    </div>
+
+    <div class="content-container"  style="width:20%">
+      <article class="main-content">
+        This is my main content!
+        <hr /> This is my main content!
+        <hr /> This is my main content!
+        <hr /> This is my main content!
+        <hr /> This is my main content!
+        <hr /> This is my main content!
+        <hr /> This is my main content!
+        <hr /> This is my main content!
+        <hr /> This is my main content!
+        <hr /> This is my main content!
+        <hr /> This is my main content!
+        <hr /> This is my main content!
+        <hr /> This is my main content!
+        <hr /> This is my main content!
+        <hr />
+      </article>
+      
+      <aside class="page-ads">
+        <div class="advert">
+          Display Ad
+        </div>
+
+        <div class="advert">
+          Ad
+        </div>
+
+        <div class="advert">
+          Ad
+        </div>
+
+        <div class="advert">
+          Ad
+        </div>
+      </aside>
+    </div>
+  </div>
+
+  <footer>
+    <small>&copy West Wind Technologies, 2016</small>
+  </footer>
+
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='https://rawgit.com/RickStrahl/jquery-resizable/master/src/jquery-resizable.js'></script>
+
+  
+
+    <script  src="js/index.js"></script>
+
+
+
+
+</body>
+
+</html>
