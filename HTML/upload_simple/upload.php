192691096da7f6c955bf7e0a493b0d217b1d98db
vincseize <vincseize@gmail.com>
2018-11-15 22:57:02 +0100

    Create upload.php

---
+++ HTML/upload_simple/upload.php
@@ -0,0 +1,26 @@
+
+<?php
+// Dans les versions de PHP antiéreures à 4.1.0, la variable $HTTP_POST_FILES
+// doit être utilisée à la place de la variable $_FILES.
+
+$uploaddir = dirname(__FILE__).'/uploads/';
+// $uploaddir = '';
+echo $uploaddir;
+echo '<pre>';
+$uploadfile = $uploaddir . basename($_FILES['fileToUpload']['name']);
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $uploadfile)) {
+    echo "Le fichier est valide, et a été téléchargé
+           avec succès. Voici plus d'informations :\n";
+} else {
+    echo "Attaque potentielle par téléchargement de fichiers.
+          Voici plus d'informations :\n";
+}
+
+echo 'Voici quelques informations de débogage :';
+print_r($_FILES);
+
+echo '</pre>';
+
+?>
