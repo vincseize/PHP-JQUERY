8e33220a8670de7df0f7b7bd73063d238e5dccc6
vincseize <vincseize@gmail.com>
2019-03-08 15:50:49 +0100

    Update phpConsoleLog.php

--- phpConsoleLog.php
+++ phpConsoleLog.php
@@ -8,3 +8,10 @@ function jsLogs($data) {
     # exit();
 }
 ?>
+<!-- // usage
+# For Array
+jsLogs(array("test1", "test2")); # PHP: ["test1","test2"]
+# For Object
+jsLogs(array("test1"=>array("subtest1", "subtest2"))); #PHP: {"test1":["subtest1","subtest2"]}
+# For String
+jsLogs("testing string"); #PHP: testing string -->
