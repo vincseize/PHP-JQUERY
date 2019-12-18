25a7de3a4174fb2e25248806fcc096881dec18ca
vincseize <vincseize@gmail.com>
2019-12-18 22:30:15 +0100

    refactoring, minify

--- table_pagination.php
+++ table_pagination.php
@@ -1,5 +1,6 @@
 <?php
 
+    // DB
     $host = 'localhost';
     $username = 'root';
     $password = '';
@@ -7,6 +8,15 @@
     $table = 'clients';
     $pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
 
+    // VARS
+    $page_index = $_SERVER["PHP_SELF"];
+    $limit = 15; 
+    $n_btn_number = 4; // n*2 +1 (without first and last) max visible btn
+    $icon_before = "&#60;";
+    $icon_next = "&#62;";
+    $icon_etc = "...";
+
+    // DONT TOUCH !Important
     function rows_count($pdo, $table){
         $sth = $pdo->prepare("SELECT COUNT(*) AS result FROM $table");
         $sth->execute(); 
@@ -28,32 +38,17 @@
         return $table_fields;
     }
 
-    $page_index = $_SERVER["PHP_SELF"];
-    $limit = 15; 
-    $n_btn_number = 4; // n*2 +1 (without first and last) max visible btn
-    $before_icon = "&#60;";
-    $next_icon = "&#62;";
-    $etc = "...";
-
-    // dont touch
     $page = (isset($_GET['page'])) ? $_GET['page'] : 1;
     $limit_start = ($page - 1) * $limit;
-
     $datas = select_table($pdo, $table, $limit_start, $limit);
-    $rows_count =rows_count($pdo, $table);
+    $rows_count = rows_count($pdo, $table);
     $n_pages_result = ceil($rows_count / $limit);
-    $link_active = "";
-
-    $start_number = ($page > $n_btn_number) ? $page - $n_btn_number : 1; // Untuk awal link member
-    $end_number = ($page < ($n_pages_result - $n_btn_number)) ? $page + $n_btn_number : $n_pages_result; // Untuk akhir link number
-
+    $start_number = ($page > $n_btn_number) ? $page - $n_btn_number : 1;
+    $end_number = ($page < ($n_pages_result - $n_btn_number)) ? $page + $n_btn_number : $n_pages_result;
     $result_numbers = array();
     for ($i = $start_number; $i <= $end_number; $i++) {
         array_push($result_numbers,$i);
     }
-
-    $sth = $pdo->prepare("DESCRIBE $table");
-    $sth->execute();
     $table_fields = table_fields($pdo, $table);
 
 ?>
@@ -120,47 +115,73 @@
         <ul class="pagination">
 
             <?php
-                // ------------------------ LINK BTN NUMBERS
+                // ------------------------ BTN NUMBERS
                 
-                // LINK PAGE FIRST AND PREV
-                if($page==1){$link_active = ' class="active"';}
+                // NUMBER PAGE PREV
+                $link_active = '';
                 if ($page == 1) { 
-                    echo "<li class='disabled'><a>$before_icon</a></li>";
+                    $class_disabled = "class='disabled'";  
+                    if($rows_count==0){$class_disabled = "class='disabled'";}
+                    echo "<li $link_active class='disabled'><a>$icon_before</a></li>";
                 } else { 
+                    $class_disabled = '';
+                    if($rows_count==0){$class_disabled = "class='disabled'";}
                     $link_prev = ($page > 1) ? $page - 1 : 1;
-                    echo "<li><a href='$page_index?page=$link_prev'>$before_icon</a></li>";
+                    echo "<li $link_active $class_disabled><a href='$page_index?page=$link_prev'>$icon_before</a></li>";
                 }
 
-                // LINK FIRST PAGE NUMBER
+                // NUMBER FIRST PAGE
+                if($page==1){$link_active = ' class="active"';}else{$link_active = '';}
                 echo "<li $link_active><a href='$page_index?page=1'>1</a></li>";
+
+                // NUMBER icon_etc PAGE
+                $link_active = '';
+                $class_disabled = '';
+                $start_number_minus1 = $start_number-1;
                 if ($start_number+1 >= $n_btn_number) { 
-                    $start_number_minus1 = $start_number-1;
-                    echo "<li><a href='$page_index?page=$start_number_minus1'>$etc</a></li>";
+                    echo "<li $link_active $class_disabled><a href='$page_index?page=$start_number_minus1'>$icon_etc</a></li>";
                 }
 
-                // LINK NUMBERS
-                for ($i = $start_number; $i <= $end_number; $i++) {
-                    $link_active = ($page == $i) ? ' class="active"' : '';   
-                    if ($i != '1' && $i != $n_pages_result) {
-                        echo "<li $link_active><a href='$page_index?page=$i'>$i</a></li>";
+                // NUMBERS PAGE
+                $class_disabled = '';
+                if($rows_count!=0 && $rows_count>$limit){
+                    for ($i = $start_number; $i <= $end_number; $i++) {
+                        $link_active = ($page == $i) ? ' class="active"' : '';   
+                        if ($i != '1' && $i != $n_pages_result) {
+                            echo "<li $link_active $class_disabled><a href='$page_index?page=$i'>$i</a></li>";
+                        }
                     }
                 }
 
-                // LINK END PAGE NUMBERS
+                // NUMBER icon_etc PAGE
+                $class_disabled = '';
                 if($page==$n_pages_result){$link_active = ' class="active"';}
                 $end_number_max1 = $end_number+1;
                 if ($end_number_max1 < $n_pages_result) {
-                    echo "<li><a href='$page_index?page=$end_number_max1'>$etc</a></li>";
+                    $link_active = '';
+                    echo "<li $link_active $class_disabled><a href='$page_index?page=$end_number_max1'>$icon_etc</a></li>";
                 }
-                echo "<li $link_active><a href='$page_index?page=$n_pages_result'>$n_pages_result</a></li>";
 
-                // LINK NEXT AND LAST
-                if ($page == $n_pages_result) {
-                    echo "<li class='disabled'><a>$next_icon</a></li>";
-                } else {
-                    $link_next = ($page < $n_pages_result) ? $page + 1 : $n_pages_result;
-                    echo "<li><a href='$page_index?page=$link_next'>$next_icon</a></li>";
+                // NUMBER END PAGE
+                if($rows_count!=0 && $rows_count>$limit){
+                    $class_disabled = '';
+                    if($page==$n_pages_result){$link_active = ' class="active"';}else{$link_active = '';}
+                    echo "<li $link_active $class_disabled><a href='$page_index?page=$n_pages_result'>$n_pages_result</a></li>";
                 }
+                // NUMBER PAGE NEXT 
+                if ($page == $n_pages_result) {
+                    $link_active = '';
+                    $class_disabled = "class='disabled'";  
+                    if($rows_count==0){$class_disabled = "class='disabled'";}
+                    echo "<li $link_active $class_disabled><a>$icon_next</a></li>";
+                } else {
+                    $link_active = '';
+                    $class_disabled = '';
+                    if($rows_count==0){$class_disabled = "class='disabled'";}
+                    $link_next = ($page < $n_pages_result) ? $page + 1 : $n_pages_result;
+                    echo "<li $link_active $class_disabled><a href='$page_index?page=$link_next'>$icon_next</a></li>";
+                }
+
             ?>
 
         </ul>
