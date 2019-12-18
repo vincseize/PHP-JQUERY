6ab9cd487d7999bf2e8ebd95b96595addc6cbbee
vincseize <vincseize@gmail.com>
2019-12-18 21:07:28 +0100

    minification

--- table_pagination.php
+++ table_pagination.php
@@ -1,54 +1,60 @@
 <?php
 
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'booking_vuejs';
-$table = 'clients';
-$pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'booking_vuejs';
+    $table = 'clients';
+    $pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
 
-function rows_count($pdo, $table){
-    $sql = $pdo->prepare("SELECT COUNT(*) AS result FROM $table");
-    $sql->execute(); 
-    $rows = $sql->fetch();
-    $rows_count = $rows['result'];
-    return $rows_count;
-}
+    function rows_count($pdo, $table){
+        $sth = $pdo->prepare("SELECT COUNT(*) AS result FROM $table");
+        $sth->execute(); 
+        $rows = $sth->fetch();
+        $rows_count = $rows['result'];
+        return $rows_count;
+    }
 
-function select_table($pdo, $table, $limit_start, $limit){
-    $sql = $pdo->prepare("SELECT * FROM $table LIMIT ".$limit_start.",".$limit);
-    $sql->execute(); 
-    return $sql;
-}
+    function select_table($pdo, $table, $limit_start, $limit){
+        $sth = $pdo->prepare("SELECT * FROM $table LIMIT ".$limit_start.",".$limit);
+        $sth->execute(); 
+        return $sth;
+    }
 
-$page_index = $_SERVER["PHP_SELF"];
-$limit = 15; 
-$n_btn_number = 4; // n*2 +1 (without first and last) max visible btn
-$before_icon = "&#60;";
-$next_icon = "&#62;";
-$etc = "...";
+    function table_fields($pdo, $table){
+        $sth = $pdo->prepare("DESCRIBE $table");
+        $sth->execute();
+        $table_fields = $sth->fetchAll(PDO::FETCH_COLUMN);
+        return $table_fields;
+    }
 
-// dont touch
-$page = (isset($_GET['page'])) ? $_GET['page'] : 1;
-$limit_start = ($page - 1) * $limit;
+    $page_index = $_SERVER["PHP_SELF"];
+    $limit = 15; 
+    $n_btn_number = 4; // n*2 +1 (without first and last) max visible btn
+    $before_icon = "&#60;";
+    $next_icon = "&#62;";
+    $etc = "...";
 
-$datas = select_table($pdo, $table, $limit_start, $limit);
-$rows_count =rows_count($pdo, $table);
-$n_pages_result = ceil($rows_count / $limit);
-$link_active = "";
+    // dont touch
+    $page = (isset($_GET['page'])) ? $_GET['page'] : 1;
+    $limit_start = ($page - 1) * $limit;
 
-$start_number = ($page > $n_btn_number) ? $page - $n_btn_number : 1; // Untuk awal link member
-$end_number = ($page < ($n_pages_result - $n_btn_number)) ? $page + $n_btn_number : $n_pages_result; // Untuk akhir link number
+    $datas = select_table($pdo, $table, $limit_start, $limit);
+    $rows_count =rows_count($pdo, $table);
+    $n_pages_result = ceil($rows_count / $limit);
+    $link_active = "";
 
-$result_numbers = array();
-for ($i = $start_number; $i <= $end_number; $i++) {
-    array_push($result_numbers,$i);
-}
+    $start_number = ($page > $n_btn_number) ? $page - $n_btn_number : 1; // Untuk awal link member
+    $end_number = ($page < ($n_pages_result - $n_btn_number)) ? $page + $n_btn_number : $n_pages_result; // Untuk akhir link number
 
+    $result_numbers = array();
+    for ($i = $start_number; $i <= $end_number; $i++) {
+        array_push($result_numbers,$i);
+    }
 
-$STH = $pdo->prepare("DESCRIBE $table");
-$STH->execute();
-$table_fields = $STH->fetchAll(PDO::FETCH_COLUMN);
+    $sth = $pdo->prepare("DESCRIBE $table");
+    $sth->execute();
+    $table_fields = table_fields($pdo, $table);
 
 ?>
 
@@ -89,8 +95,8 @@ $table_fields = $STH->fetchAll(PDO::FETCH_COLUMN);
                         }
                     ?>
                 </tr>
-                <?php
 
+                <?php
                 $no = $limit_start + 1; 
                 while ($data = $datas->fetch()) { 
                 ?>
@@ -106,98 +112,57 @@ $table_fields = $STH->fetchAll(PDO::FETCH_COLUMN);
                 $no++; 
                 }
                 ?>
+
             </table>
         <div>
 
+        <!-- ------------------------------- PAGINATION --------------------------------------------- -->
         <ul class="pagination">
 
-            <!-- LINK BTN NUMBERS -->
             <?php
+                // ------------------------ LINK BTN NUMBERS
+                
+                // LINK PAGE FIRST AND PREV
                 if($page==1){$link_active = ' class="active"';}
-            ?>
-
-
-            <!-- LINK PAGE FIRST AND PREV -->
-            <?php   
                 if ($page == 1) { 
-            ?>
-            <li class="disabled"><a>&#60;</a></li>
+                    echo "<li class='disabled'><a>$before_icon</a></li>";
+                } else { 
+                    $link_prev = ($page > 1) ? $page - 1 : 1;
+                    echo "<li><a href='$page_index?page=$link_prev'>$before_icon</a></li>";
+                }
 
-            <?php
-            } else { 
-                $link_prev = ($page > 1) ? $page - 1 : 1;
-            ?>
-                <li><a href="<?php echo $page_index;?>?page=<?php echo $link_prev; ?>"><?php echo $before_icon;?></a></li>
-            <?php
-            }
-            ?>
+                // LINK FIRST PAGE NUMBER
+                echo "<li $link_active><a href='$page_index?page=1'>1</a></li>";
+                if ($start_number+1 >= $n_btn_number) { 
+                    $start_number_minus1 = $start_number-1;
+                    echo "<li><a href='$page_index?page=$start_number_minus1'>$etc</a></li>";
+                }
 
-            <!-- LINK FIRST PAGE NUMBER -->
-
-            <li <?php echo $link_active; ?>><a href="<?php echo $page_index;?>?page=1">1</a></li>
-
-            <?php
-            if ($start_number+1 >= $n_btn_number) { 
-                
-            ?>
-                <li><a href="<?php echo $page_index;?>?page=<?php echo $start_number-1;?>"><?php echo $etc;?></a></li>
-            <?php
-            }
-            ?>
-
-            <!-- LINK NUMBERS -->
-            <?php
-            for ($i = $start_number; $i <= $end_number; $i++) {
-                $link_active = ($page == $i) ? ' class="active"' : '';
-                
-            ?>
-            
-
-                <?php
-                
+                // LINK NUMBERS
+                for ($i = $start_number; $i <= $end_number; $i++) {
+                    $link_active = ($page == $i) ? ' class="active"' : '';   
                     if ($i != '1' && $i != $n_pages_result) {
-                        
-                ?>
-
-                <li <?php echo $link_active; ?>><a href="<?php echo $page_index;?>?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
-            
-                      
-            
-                <?php
+                        echo "<li $link_active><a href='$page_index?page=$i'>$i</a></li>";
                     }
+                }
 
+                // LINK END PAGE NUMBERS
+                if($page==$n_pages_result){$link_active = ' class="active"';}
+                $end_number_max1 = $end_number+1;
+                if ($end_number_max1 < $n_pages_result) {
+                    echo "<li><a href='$page_index?page=$end_number_max1'>$etc</a></li>";
+                }
+                echo "<li $link_active><a href='$page_index?page=$n_pages_result'>$n_pages_result</a></li>";
 
-            }
-            ?>
-
-            <!-- LINK END PAGE NUMBERS -->
-            <?php
-                if ($end_number+1 < $n_pages_result) {
-                ?>
-                <li><a href="<?php echo $page_index;?>?page=<?php echo $end_number+1;?>"><?php echo $etc;?></a></li>
-            <?php
+                // LINK NEXT AND LAST
+                if ($page == $n_pages_result) {
+                    echo "<li class='disabled'><a>$next_icon</a></li>";
+                } else {
+                    $link_next = ($page < $n_pages_result) ? $page + 1 : $n_pages_result;
+                    echo "<li><a href='$page_index?page=$link_next'>$next_icon</a></li>";
                 }
             ?>
 
-
-            <?php
-                if($page==$n_pages_result){$link_active = ' class="active"';}
-            ?>
-            <li <?php echo $link_active; ?>><a href="<?php echo $page_index;?>?page=<?php echo $n_pages_result;?>"><?php echo $n_pages_result;?></a></li>
-
-            <!-- LINK NEXT AND LAST -->
-            <?php
-            if ($page == $n_pages_result) {
-            ?>
-                <li class="disabled"><a href="#"><?php echo $next_icon;?></a></li>
-            <?php
-            } else {
-                $link_next = ($page < $n_pages_result) ? $page + 1 : $n_pages_result;
-            ?>
-                <li><a href="<?php echo $page_index;?>?page=<?php echo $link_next; ?>"><?php echo $next_icon;?></a></li>
-            <?php
-            }
-            ?>
         </ul>
     </div>
 </body>
