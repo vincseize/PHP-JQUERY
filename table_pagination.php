59584e162513a56e60a7977dfc01394b457f33ab
vincseize <vincseize@gmail.com>
2019-12-18 20:16:41 +0100

    proprification

--- table_pagination.php
+++ table_pagination.php
@@ -7,7 +7,6 @@ $database = 'booking_vuejs';
 $table = 'clients';
 $pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
 
-
 function rows_count($pdo, $table){
     $sql = $pdo->prepare("SELECT COUNT(*) AS result FROM $table");
     $sql->execute(); 
@@ -22,11 +21,6 @@ function select_table($pdo, $table, $limit_start, $limit){
     return $sql;
 }
 
-
-
-
-
-
 $page_index = $_SERVER["PHP_SELF"];
 $limit = 15; 
 $n_btn_number = 4; // n*2 +1 (without first and last) max visible btn
@@ -34,17 +28,14 @@ $before_icon = "&#60;";
 $next_icon = "&#62;";
 $etc = "...";
 
-
 // dont touch
 $page = (isset($_GET['page'])) ? $_GET['page'] : 1;
 $limit_start = ($page - 1) * $limit;
 
-
-
 $datas = select_table($pdo, $table, $limit_start, $limit);
 $rows_count =rows_count($pdo, $table);
 $n_pages_result = ceil($rows_count / $limit);
-
+$link_active = "";
 
 $start_number = ($page > $n_btn_number) ? $page - $n_btn_number : 1; // Untuk awal link member
 $end_number = ($page < ($n_pages_result - $n_btn_number)) ? $page + $n_btn_number : $n_pages_result; // Untuk akhir link number
@@ -54,7 +45,13 @@ for ($i = $start_number; $i <= $end_number; $i++) {
     array_push($result_numbers,$i);
 }
 
+
+$STH = $pdo->prepare("DESCRIBE $table");
+$STH->execute();
+$table_fields = $STH->fetchAll(PDO::FETCH_COLUMN);
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -86,11 +83,11 @@ for ($i = $start_number; $i <= $end_number; $i++) {
             <table class="table table-bordered table-striped table-hover">
                 <tr>
                     <th class="text-center"><?php echo $rows_count;?></th>
-                    <th>id</th>
-                    <th>nom</th>
-                    <th>email</th>
-                    <th>created</th>
-                    <th>updated</th>
+                    <?php
+                        foreach($table_fields as $field){
+                            echo "<th>$field</th>";
+                        }
+                    ?>
                 </tr>
                 <?php
 
@@ -99,11 +96,11 @@ for ($i = $start_number; $i <= $end_number; $i++) {
                 ?>
                     <tr>
                         <td class="align-middle text-center"><?php echo $no; ?></td>
-                        <td class="align-middle"><?php echo $data['id']; ?></td>
-                        <td class="align-middle"><?php echo $data['nom']; ?></td>
-                        <td class="align-middle"><?php echo $data['email']; ?></td>
-                        <td class="align-middle"><?php echo $data['created']; ?></td>
-                        <td class="align-middle"><?php echo $data['updated']; ?></td>
+                        <?php
+                            foreach($table_fields as $field){
+                                echo "<td class='align-middle'>$data[$field]</td>";
+                            }
+                        ?>
                     </tr>
                 <?php
                 $no++; 
@@ -115,6 +112,9 @@ for ($i = $start_number; $i <= $end_number; $i++) {
         <ul class="pagination">
 
             <!-- LINK BTN NUMBERS -->
+            <?php
+                if($page==1){$link_active = ' class="active"';}
+            ?>
 
 
             <!-- LINK PAGE FIRST AND PREV -->
@@ -133,10 +133,12 @@ for ($i = $start_number; $i <= $end_number; $i++) {
             ?>
 
             <!-- LINK FIRST PAGE NUMBER -->
-            <li><a href="<?php echo $page_index;?>?page=1">1</a></li>
+
+            <li <?php echo $link_active; ?>><a href="<?php echo $page_index;?>?page=1">1</a></li>
 
             <?php
             if ($start_number+1 >= $n_btn_number) { 
+                
             ?>
                 <li><a href="<?php echo $page_index;?>?page=<?php echo $start_number-1;?>"><?php echo $etc;?></a></li>
             <?php
@@ -146,12 +148,15 @@ for ($i = $start_number; $i <= $end_number; $i++) {
             <!-- LINK NUMBERS -->
             <?php
             for ($i = $start_number; $i <= $end_number; $i++) {
-                $link_active = ($page == $i) ? 'class="active"' : '';
+                $link_active = ($page == $i) ? ' class="active"' : '';
+                
             ?>
             
 
                 <?php
+                
                     if ($i != '1' && $i != $n_pages_result) {
+                        
                 ?>
 
                 <li <?php echo $link_active; ?>><a href="<?php echo $page_index;?>?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
@@ -174,7 +179,11 @@ for ($i = $start_number; $i <= $end_number; $i++) {
                 }
             ?>
 
-            <li><a href="<?php echo $page_index;?>?page=<?php echo $n_pages_result;?>"><?php echo $n_pages_result;?></a></li>
+
+            <?php
+                if($page==$n_pages_result){$link_active = ' class="active"';}
+            ?>
+            <li <?php echo $link_active; ?>><a href="<?php echo $page_index;?>?page=<?php echo $n_pages_result;?>"><?php echo $n_pages_result;?></a></li>
 
             <!-- LINK NEXT AND LAST -->
             <?php
