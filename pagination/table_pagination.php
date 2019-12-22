3ef8c333d81ce63a390059ec924f2fa185c37b8b
vincseize <vincseize@gmail.com>
2019-12-22 15:14:00 +0100

    Update table_pagination.php

--- pagination/table_pagination.php
+++ pagination/table_pagination.php
@@ -3,7 +3,6 @@
     require "table_pagination_pdo.class.php";
     require "table_pagination.class.php";
 
-    // table 
     function select_table($pdo, $table, $limit_start, $pgn_limit){
         $sth = $pdo->prepare("SELECT * FROM $table ORDER by id DESC LIMIT ".$limit_start.",".$pgn_limit);
         $sth->execute(); 
@@ -21,18 +20,17 @@
         $sth = $pdo->prepare("SELECT COUNT(*) AS result FROM $table");
         $sth->execute(); 
         $rows = $sth->fetch();
-        // $pgn_rCount = $rows['result'];
         return $rows['result'];
     }
 
-    // 
+    // TABLE
     $pdo = new PDOConfig('booking_vuejs');
     $table = 'clients';
     $table_fields = table_fields($pdo, $table);
     $n_results_array = ['5','10','15','25','50']; 
     $pgn_limit = 15; 
-    $pgn_paramPage   = 'page'; // url var
-    $pgn_paramRes = 'n_result'; // url var
+    $pgn_paramPage   = 'page';
+    $pgn_paramRes = 'n_result';
     if(isset($_GET[$pgn_paramRes])){$pgn_limit = $_GET[$pgn_paramRes]; }
     $pgn_page = (isset($_GET[$pgn_paramPage])) ? $_GET[$pgn_paramPage] : 1;
     $limit_start = ($pgn_page - 1) * $pgn_limit;
@@ -110,19 +108,16 @@
         <!-- ------------------------------- PAGINATION -->
         <?php              
             // VARS
-            $pgn_dfltLimit = 15; // n results
+            $pgn_dfltLimit = 15;
             $pgn_rCount    = rows_count($pdo, $table);
-            $pgn_paramPage = 'page'; // url var
-            $pgn_paramRes  = 'n_result'; // url var
-            $pgn_nBtns     = 4; // tot max visible btn = n*2 +1 (without first and last)
+            $pgn_paramPage = 'page';
+            $pgn_paramRes  = 'n_result';
+            $pgn_nBtns     = 4;
             $pgn_ics       = array("icon_before"=>"&#60;","icon_etc"=>"...","icon_next"=>"&#62;");
-            // |icon_before|...|1|2|3|4|center nb|5|6|7|8|...|icon_next|
-
+            // DONT TOUCH
             $pgn_page      = (isset($_GET[$pgn_paramPage])) ? $_GET[$pgn_paramPage] : 1;
-            $pgn_limit      = $pgn_limit;
-            if(isset($_GET[$pgn_paramRes])){$pgn_limit = $_GET[$pgn_paramRes]; }
-
-
+            if(isset($_GET[$pgn_paramRes])){$pgn_dfltLimit = $_GET[$pgn_paramRes]; }
+            // Let's go
             new Pagination($pgn_page,$pgn_dfltLimit,$pgn_rCount,$pgn_nBtns,$pgn_ics,$pgn_paramPage,$pgn_paramRes);        
         ?>
         <script src="table_pagination.js"></script>
