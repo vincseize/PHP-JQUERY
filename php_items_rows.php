fbd74fae3234cdec52da3deac34a9f91e651c5b3
vincseize <vincseize@gmail.com>
2020-03-27 19:16:43 +0100

    Update php_items_rows.php

--- php_items_rows.php
+++ php_items_rows.php
@@ -2,7 +2,7 @@
     //   $posts = [['id' => 1], ['id' => 2] ...];
       $posts = $images;
 
-      $postChunks = array_chunk($posts, 6); // 6 is used to have 4 items in a row
+      $postChunks = array_chunk($posts, 6); // 6 is used to have 6 items in a row
       foreach ($postChunks as $posts) {
         print_r("<br>---- BEGIN container<br>");
               foreach ($posts as $img) {
