54aca43cbffea9c9eda450eeebe209d64577d926
vincseize <vincseize@gmail.com>
2020-09-21 16:18:42 +0200

    Add files via upload

---
+++ JSON/crudJson/_form.php
@@ -0,0 +1,70 @@
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h3>
+                <?php if ($user['id']): ?>
+                    Id <?php echo $lastId; ?> | Update user 
+                    <b><?php echo htmlentities($user['name'], ENT_QUOTES) ?>
+                </b>
+                <?php else: ?>
+                    Create new User
+                <?php endif ?>
+            </h3>
+        </div>
+        <div class="card-body">
+
+            <form method="POST" enctype="multipart/form-data"
+                  action="">
+                <div class="form-group">
+                    <label>Name</label>
+                    <input name="name" value="<?php echo htmlentities($user['name'], ENT_QUOTES) ?>"
+                           class="form-control <?php echo $errors['name'] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?php echo  $errors['name'] ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input name="username" value="<?php echo $user['username'] ?>"
+                           class="form-control <?php echo $errors['username'] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?php echo  $errors['username'] ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Email</label>
+                    <input name="email" value="<?php echo $user['email'] ?>"
+                           class="form-control  <?php echo $errors['email'] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?php echo  $errors['email'] ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Phone</label>
+                    <input name="phone" value="<?php echo $user['phone'] ?>"
+                           class="form-control  <?php echo $errors['phone'] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?php echo  $errors['phone'] ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Website</label>
+                    <input name="website" value="<?php echo $user['website'] ?>"
+                           class="form-control  <?php echo $errors['website'] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?php echo  $errors['website'] ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Image</label>
+                    <input name="picture" type="file" class="form-control-file">
+                </div>
+
+
+                <a class="btn btn-default" href="index.php">Cancel</a>
+                <button class="btn btn-success">Submit</button>
+                
+            </form>
+        </div>
+    </div>
+</div>
